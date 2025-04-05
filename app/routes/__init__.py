c49ec5472bcd42188858aa3f8c9d6aa3bd1b7c0c
David Vaz Teixeira <dvt4qb@gmail.com>
2025-04-05 05:21:17 -0300

    rename view blueprint alias

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,8 +1,8 @@
-from app.routes.view import bp as routes_blueprint
+from app.routes.view import bp as view_blueprint
 from app.routes.api import bp as api_blueprint
 from app.routes.root import bp as root_blueprint
 
 def draw_routes_for(app):
     app.register_blueprint(root_blueprint, url_prefix='/')
     app.register_blueprint(api_blueprint, url_prefix='/ita/exec')
-    app.register_blueprint(routes_blueprint, url_prefix='/ita/view')
+    app.register_blueprint(view_blueprint, url_prefix='/ita/view')
