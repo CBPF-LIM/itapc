7a6a5bfe836180027f8ed5a9408590fd0b97ed83
David Vaz Teixeira <dvt4qb@gmail.com>
2025-04-08 01:45:32 -0300

    add settings CRUD

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -4,6 +4,7 @@ from app.routes.view.table import bp as view_table_blueprint
 from app.routes.view.chart import bp as view_chart_blueprint
 from app.routes.view.logs import bp as view_logs_blueprint
 from app.routes.view.tools import bp as view_tools_blueprint
+from app.routes.view.settings import bp as view_settings_blueprint
 
 from app.routes.api import bp as api_blueprint
 
@@ -14,5 +15,6 @@ def draw_routes_for(app):
     app.register_blueprint(view_chart_blueprint, url_prefix='/view/chart')
     app.register_blueprint(view_logs_blueprint, url_prefix='/view/logs')
     app.register_blueprint(view_tools_blueprint, url_prefix='/view/tools')
+    app.register_blueprint(view_settings_blueprint, url_prefix='/view/settings')
 
     app.register_blueprint(api_blueprint, url_prefix='/api')
--- app/routes/view/settings.py
+++ app/routes/view/settings.py
@@ -1,8 +1,73 @@
-from flask import Blueprint, redirect, render_template, url_for
+from flask import Blueprint, redirect, render_template, url_for, request, flash
 from flasktools import *
+from ita.models import Setting
+import json
 
 bp = auto_blueprint()
 
 @bp.route('/', methods=['GET'])
 def index():
+    settings = Setting.select()
+    return render('index', settings=settings)
+
+@bp.route('/<int:id>/edit', methods=['GET'])
+def edit(id):
+    setting = Setting.select().where(Setting.id == id).first()
+    return render('edit', setting=setting)
+
+@bp.route('/new', methods=['GET'])
+def new():
+    setting = Setting()
+    setting.config = {}
+    return render('new', setting=setting)
+
+@bp.route('/', methods=['POST'])
+def create():
+    setting = Setting()
+
+    keys = request.form.getlist('keys[]')
+    values = request.form.getlist('values[]')
+
+    new_config = generate_config(keys, values)
+    if new_config is None:
+        return redirect(action_for('.edit', id=id))
+
+    setting.config = new_config
+    setting.save()
+
     return redirect(action_for('.index'))
+
+@bp.route('/<int:id>/update', methods=['POST'])
+def update(id):
+    setting = Setting.select().where(Setting.id == id).first()
+
+    keys = request.form.getlist('keys[]')
+    values = request.form.getlist('values[]')
+
+    new_config = generate_config(keys, values)
+    if new_config is None:
+        return redirect(action_for('.edit', id=id))
+
+    setting.config = new_config
+    setting.save()
+
+    return redirect(action_for('.index'))
+
+def generate_config(keys, values):
+    config = {}
+
+    for k, v in zip(keys, values):
+        if k == '' and v != '':
+            flash('Key cannot be empty', 'danger')
+            return None
+
+        if k == '':
+            continue
+
+        try:
+            config[k] = v
+        except json.JSONDecodeError:
+            flash(f'Invalid JSON for key-value pair: {k}: {v}', 'danger')
+            return None
+
+    return config
