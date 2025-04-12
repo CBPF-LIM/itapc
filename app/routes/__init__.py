48fb36e7f869fcc55715356b818c2362e5b134c0
David Vaz Teixeira <dvt4qb@gmail.com>
2025-04-12 00:03:21 -0300

    move table and chart to experiment

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,20 +1,18 @@
 from app.routes.view.root import bp as view_root_blueprint
 from app.routes.view.api import bp as view_api_blueprint
-from app.routes.view.table import bp as view_table_blueprint
-from app.routes.view.chart import bp as view_chart_blueprint
 from app.routes.view.logs import bp as view_logs_blueprint
 from app.routes.view.tools import bp as view_tools_blueprint
 from app.routes.view.settings import bp as view_settings_blueprint
+from app.routes.view.experiments import bp as view_experiments_blueprint
 
 from app.routes.api import bp as api_blueprint
 
 def draw_routes_for(app):
     app.register_blueprint(view_root_blueprint, url_prefix='/')
     app.register_blueprint(view_api_blueprint, url_prefix='/view/api')
-    app.register_blueprint(view_table_blueprint, url_prefix='/view/table')
-    app.register_blueprint(view_chart_blueprint, url_prefix='/view/chart')
     app.register_blueprint(view_logs_blueprint, url_prefix='/view/logs')
     app.register_blueprint(view_tools_blueprint, url_prefix='/view/tools')
     app.register_blueprint(view_settings_blueprint, url_prefix='/view/settings')
+    app.register_blueprint(view_experiments_blueprint, url_prefix='/view/experiments')
 
     app.register_blueprint(api_blueprint, url_prefix='/api')
---
+++ app/routes/view/experiments.py
@@ -0,0 +1,124 @@
+import os
+import json
+from flask import redirect, flash, current_app
+from ita.models import Experiment, Data, Setting
+from flasktools import *
+
+bp = auto_blueprint()
+
+# index route
+@bp.route('/')
+def index():
+    experiments = Experiment.select()
+    return render('index', experiments=experiments)
+
+
+# show route
+@bp.route('/<int:id>')
+def show(id):
+    experiment = Experiment.get_or_none(Experiment.id == id)
+
+    return render('show', experiment=experiment)
+
+
+# show_table route
+@bp.route('/table/<int:id>')
+def show_table(id):
+    experiment = Experiment.get_or_none(Experiment.id == id)
+
+    return render('show_table', experiment=experiment)
+
+
+# show_chart route
+@bp.route('/chart/<int:id>')
+def show_chart(id):
+    experiment = Experiment.get_or_none(Experiment.id == id)
+
+    return render('show_chart', experiment=experiment)
+
+
+# new route
+@bp.route('/new')
+def new():
+    experiment = Experiment()
+    experiment.header = {}
+    settings = Setting.select()
+    return render('new', experiment=experiment, settings=settings)
+
+
+# create route
+@bp.route('/', methods=['POST'])
+def create():
+    experiment = Experiment()
+    experiment.name = request.form.get('name', '')
+    experiment.header = json.loads(request.form.get('header', '{}'))
+
+    setting_id = request.form.get('setting')
+    experiment.setting = Setting.get_or_none(Setting.id == setting_id)
+
+    if experiment.setting:
+        experiment.setting_id = experiment.setting.id
+
+    if experiment.name == '':
+        flash('Experiment name is required.', 'error')
+        return redirect(url_for('.new'))
+
+    experiment.save()
+    flash('Experiment created successfully.', 'success')
+
+    return redirect(url_for('.index'))
+
+
+# edit route
+@bp.route('/<int:id>/edit')
+def edit(id):
+    experiment = Experiment.get_or_none(Experiment.id == id)
+    settings = Setting.select()
+
+    if not experiment:
+        flash('Experiment not found.', 'error')
+        return redirect(url_for('.index'))
+
+    return render('edit', experiment=experiment, settings=settings)
+
+
+# update route
+@bp.route('/<int:id>', methods=['POST'])
+def update(id):
+    experiment = Experiment.get_or_none(Experiment.id == id)
+    if not experiment:
+        flash('Experiment not found.', 'error')
+        return redirect(url_for('.index'))
+
+    experiment.name = request.form.get('name', '')
+    experiment.header = json.loads(request.form.get('header', '{}'))
+
+    setting_id = request.form.get('setting')
+    experiment.setting = Setting.get_or_none(Setting.id == setting_id)
+
+    if experiment.setting:
+        experiment.setting_id = experiment.setting.id
+
+    if experiment.name == '':
+        flash('Experiment name is required.', 'error')
+        return redirect(url_for('.edit', id=id))
+
+    experiment.save()
+    flash('Experiment updated successfully.', 'success')
+
+    return redirect(url_for('.show', id=id))
+
+
+# destroy route
+@bp.route('/<int:id>/delete', methods=['POST'])
+def delete(id):
+    experiment = Experiment.get_or_none(Experiment.id == id)
+    if experiment:
+        Data.delete().where(Data.experiment == experiment).execute()
+        experiment.delete_instance()
+
+        flash('Experiment deleted successfully.', 'success')
+    else:
+        flash('Experiment not found.', 'error')
+
+    return redirect(url_for('.index'))
