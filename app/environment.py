d3a9ddca546214f673a0b98b29c231b8bfbb47c4
David Vaz Teixeira <dvt4qb@gmail.com>
2025-04-13 02:39:36 -0300

    add dotenv

--- app/environment.py
+++ app/environment.py
@@ -1,8 +1,15 @@
 import os
 
+from dotenv import load_dotenv
+load_dotenv()
+
 ENV = {
     'host': os.getenv('ITA_HOST', '0.0.0.0'),
     'debug': os.getenv('ITA_DEBUG', 'false').lower() == 'true',
     'port': os.getenv('ITA_PORT', 6789),
-    'app_secret': os.getenv('ITA_SECRET', 'default-secret-key')
 }
+
+if(ENV['host'] == 'localhost'):
+  ENV['host'] = '127.0.0.1'
+
+print(ENV)
