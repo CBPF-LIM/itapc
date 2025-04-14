f9582c2e2a79df5ec5669324235c01789aa00066
David Vaz Teixeira <dvt4qb@gmail.com>
2025-04-14 05:02:43 -0300

    add tests to device

--- tests/models/device/test_device.py
+++ tests/models/device/test_device.py
@@ -12,12 +12,36 @@ def describe_device():
 
         assert device.name == 'Test Device'
 
-    def it_creates_an_device_2():
-        device = Device.create_with({
+  def describe_when_hash_is_not_unique():
+    def it_does_not_create_an_device():
+        Device.create_with({
             'name': 'Test Device',
             'hash': 'abcd',
         })
 
-        device = Device.find_by(hash='abcd')
+        try:
+            Device.create_with({
+                'name': 'Another Test Device',
+                'hash': 'abcd',
+            })
+        except Exception as e:
+           pass
 
-        assert device.name != 'Test Device'
+        assert Device.select().count() == 1
+
+  def describe_when_name_is_not_unique():
+    def it_creates_an_device():
+        Device.create_with({
+            'name': 'Test Device',
+            'hash': 'abcd',
+        })
+
+        try:
+            Device.create_with({
+                'name': 'Test Device',
+                'hash': 'abce',
+            })
+        except Exception as e:
+           pass
+
+        assert Device.select().count() == 2
